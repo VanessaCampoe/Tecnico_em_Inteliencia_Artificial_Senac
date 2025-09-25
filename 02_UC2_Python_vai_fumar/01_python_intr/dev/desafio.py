f3978a59762d51cb0744e161218453907951bf3b
VanessaCampoe <vanessacampoe@gmail.com>
2025-09-24 22:00:51 -0300

    aula de introcucao ao python24/09/2025

---
+++ 02_UC2_Python_vai_fumar/01_python_intr/dev/desafio.py
@@ -0,0 +1,32 @@
+"""
+comentario de muitas linhas
+ma aqui definimos as duplas 
+"""
+
+'''
+
+'''
+
+# quando for um linha
+"""
+python -m venv venv
+primeiraa venv obrigatoria 
+segunda venv (voce decide)
+Para excutar este programa 
+alt 92 contro barra 
+aqui entramos e4m seta aprta cmd e ativar select defalt profile
+venv\Scripts\activate
+
+cd. 
+dir 
+.
+..
+cd 
+dev
+venv
+
+"""
+ # saida de dados 
+print("Hello world")
+
+
