1a6a6c0fc0093f2f3d2c7298bef7ea65c660f064
VanessaCampoe <vanessacampoe@gmail.com>
2025-09-30 21:45:32 -0300

    30/09

---
+++ 02_UC2_Python_vai_fumar/dev/06_CorCalculoAreadesafio.py
@@ -0,0 +1,19 @@
+# UPPER_SNAKE_CASE E SNAKE_CASE
+# PASCAL_CASING - 
+# CONSTANTE UPPER_SNAKE_CASE
+#PEP - 8 - REGRAS
+KM2_PARA_M2 = 1_000_000
+# CONSTANTE 
+AREA_CAMPO_M2 = 100 * 60 
+# VARIAVEL - snake_case
+area_devastada_km2 = float(input("digite a area desvastada em km²"))
+# calculo 
+
+
+area_total_desmatada_m2 / area_devastada_km2 * (1000**2) #aqui e ao quadrado simbulo de ponteciaçao # esse parentese esta colocando em prioridade , ou seja manda o pythopn calculasr primeiro oo que esta dentro do ()
+area_campo_futebol = area_total_desmatada_m2 / AREA_CAMPO_M2
+
+print(f"A area desmatada {area_devastada_km2:,.0f}")
+print("A area desmatada", area_desmatada_km2, "km²")
+print("A area desmatada", area_campo_futebol," km²")
+print("A area desmatada"{}, area_campo_futebol," km²")
---
+++ 02_UC2_Python_vai_fumar/dev/07_MenuDesafio.py
@@ -0,0 +1,45 @@
+print("Menu Financeiro")
+
+print("1 - cadastrar")
+print("2 - Listar")
+print("3 - Atualizar")
+print("4 - Excluir")
+# print("")
+
+opcao = int(input("Digite a opcao:"))  # logica booliana ou relacional
+if opcao == 1: 
+    print("Cadastrar")
+
+elif opcao == 2:
+    print("Listar")
+
+elif opcao == 3:
+    print("Atualizar")
+elif opcao == 4:
+    print("Excluir")
+else:
+    print("Opcao Invalida!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# elif opcao == 3:
+#     print("")
+
+#     print("")
+#     print("")
+
+
+
+
+
