7f9fb1ce8e84183f1ac5739f9130076628e16b4d
VanessaCampoe <vanessacampoe@gmail.com>
2025-10-01 22:09:59 -0300

    ok

--- 02_UC2_Python_vai_fumar/02_dev/06_CorCalculoAreadesafio.py
+++ 02_UC2_Python_vai_fumar/02_dev/06_CorCalculoAreadesafio.py
@@ -13,7 +13,7 @@ area_devastada_km2 = float(input("digite a area desvastada em km²"))
 area_total_desmatada_m2 / area_devastada_km2 * (1000**2) #aqui e ao quadrado simbulo de ponteciaçao # esse parentese esta colocando em prioridade , ou seja manda o pythopn calculasr primeiro oo que esta dentro do ()
 area_campo_futebol = area_total_desmatada_m2 / AREA_CAMPO_M2
 
-print(f"A area desmatada {area_devastada_km2:,.0f}")
+print(f"A area desmatada-- {area_devastada_km2:,.0f}")
 print("A area desmatada", area_desmatada_km2, "km²")
 print("A area desmatada", area_campo_futebol," km²")
 print("A area desmatada"{}, area_campo_futebol," km²")
