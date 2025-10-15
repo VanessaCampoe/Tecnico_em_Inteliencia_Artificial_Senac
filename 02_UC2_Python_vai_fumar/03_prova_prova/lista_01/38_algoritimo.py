00da8780186692e1bd775dc57a42c1326039f6b8
VanessaCampoe <vanessacampoe@gmail.com>
2025-10-15 17:16:15 -0300

    Implement algorithm to calculate one-third of a number

---
+++ 02_UC2_Python_vai_fumar/03_prova_prova/lista_01/38_algoritimo.py
@@ -0,0 +1,4 @@
+# Algoritmo 38 – Ler um número real e imprimir a parte terça deste número
+n = float(input("Digite um número real: "))
+parte_terca = n / 3
+print("A parte terça do número é:", parte_terca)
